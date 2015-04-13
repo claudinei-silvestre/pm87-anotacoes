ea3668858cf173d8aa46b89cb6375d4af0b6373f
Claudinei Silvestre <agil5214@caelumBSB02-02>
2015-04-13 12:09:52 -0300

    mais uma mensagem

--- Teste2Git.java
+++ Teste2Git.java
@@ -2,5 +2,6 @@ public class TesteGit {
 	public static void main(String[] args) {
 		System.out.println("Exemplo de teste de uso do git");
 		System.out.println("Exemplo de teste de uso do git2");
+		System.out.println("Trabalhando com branch");
 	}
 }
